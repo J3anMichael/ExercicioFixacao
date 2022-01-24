105d1af4eebc48385eb796c1eb8208003919220e
Jean Michael <jeanmmclaudino@gmail.com>
2022-01-24 20:54:27 -0300

    Add project files.

---
+++ ExercicioFixacao/ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioFixacao
+{
+    internal class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        //Construtor 1
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0; //Por padrão o valor já é zero, mas por questões didáticas irei colocar.
+        }
+
+        //Construtor 2
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular) // aqui estamos refêrenciando o construtor 1
+        {
+            Saldo = depositoInicial;
+        }
+
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        // Aqui vamos será mostrados os dados completos do titular da conta
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Titular + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioFixacao/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ExercicioFixacao;
+using System.Globalization;
+
+
+namespace Exercicio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Instanciando nossa classe ContaBancaria
+            ContaBancaria conta;
+            //code ..
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o Titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string resp = Console.ReadLine();
+            if(resp == "s" || resp == "S")
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial); // instanciamos com os valores que iremos alterar
+
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular); // instancia que trará os valores padrão e nosso ToString() da classe ContaBancaria;
+            }
+
+
+            
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            //Deposito
+            Console.WriteLine();
+            Console.Write("Entre com um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            //Saque
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
